7dd22c88c6682232a1cdbd77db6c55fb04e1fc76
Ivan <xpynkostb@gmail.com>
2022-04-12 18:07:37 +0300

    fixing font awesome icons v4

--- client/main.jsx
+++ client/main.jsx
@@ -4,6 +4,8 @@ import Root from './config/root'
 
 import './assets/scss/main.scss'
 
+<script src='https://kit.fontawesome.com/a076d05399.js' />
+
 const target = document.getElementById('root')
 
 ReactDOM.render(<Root />, target)
