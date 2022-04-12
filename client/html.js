2db3955d0a91bfde1ba5c5374ed2caf9fcefbd22
Ivan <xpynkostb@gmail.com>
2022-04-12 17:53:04 +0300

    fixing font awesome icons v3

--- client/html.js
+++ client/html.js
@@ -7,6 +7,11 @@ const Html = ({ body }) => {
       <meta charset="UTF-8">
       <link rel="stylesheet" type="text/css" href="/css/main.css" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link
+      rel="icon"
+      href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAADElEQVQI12P4//8/AAX+Av7czFnnAAAAAElFTkSuQmCC"
+    />
+    <script src='https://kit.fontawesome.com/a076d05399.js' type="application/json" crossorigin='anonymous'></script>
     </head>
     <body>
       <script type="text/javascript" src="/js/main.bundle.js?v=COMMITHASH"></script>
